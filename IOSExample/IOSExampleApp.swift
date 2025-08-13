2c52812f653411a0c257ca172de438fa86b08a8b
anowar72 <anowar.hossain.riseuplabs@gmail.com>
2025-08-13 15:35:48 +0600

    Initial Commit

---
+++ IOSExample/IOSExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSExampleApp.swift
+//  IOSExample
+//
+//  Created by mac on 13/8/25.
+//
+
+import SwiftUI
+
+@main
+struct IOSExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
