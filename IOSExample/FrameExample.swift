c22dd6bacfb10ef02852c85c0f15969910301c14
Anowar hossain <hossainanowar72@gmail.com>
2025-08-28 16:08:12 +0600

    frame example added

---
+++ IOSExample/FrameExample.swift
@@ -0,0 +1,24 @@
+//
+//  FrameExample.swift
+//  IOSExample
+//
+//  Created by mac on 27/8/25.
+//
+
+import SwiftUI
+
+struct FrameExample :View {
+    var body: some View {
+        Text("Hello World")
+            .background(.red)
+//            .frame(width: 300, height: 200, alignment: .center)
+            .background(.green)
+    }
+}
+
+struct FrameExample_Preview:PreviewProvider{
+    static var previews: some View{
+        FrameExample()
+    }
+}
+
